5e21b93885d7ae2f2e2ced13322b14074ff4bf74
cfaloon <cfaloon@gmail.com>
2018-01-01 17:45:17 -0500

    Start displaying some Feeds!

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,22 @@
 class Feed < ApplicationRecord
+
+  def rss_response
+    @rss_response ||= HTTParty.get(self.url)['rss']
+  end
+
+  def items
+    self.rss_response['channel']['item']
+  end
+
+  %w(url title link).each do |attribute|
+    define_method "image_#{attribute}" do
+      self.rss_response['channel']['image'][attribute]
+    end
+  end
+
+  %w(title description).each do |method_name|
+    define_method method_name do
+      self.rss_response['channel'][method_name]
+    end
+  end
 end
