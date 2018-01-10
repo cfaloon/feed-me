c1c68c017e691a4fbaebbefd88239adbfe663ed2
cfaloon <cfaloon@gmail.com>
2018-01-10 16:37:12 -0500

    For now, select only those Feed Items that have a pubDate for the stream.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def stream
-    @items = current_user.feeds.map(&:items).flatten.sort { |x,y| y.pubDate <=> x.pubDate }
+    @items = current_user.feeds.map(&:items).flatten.select(&:pubDate).sort { |x,y| y.pubDate <=> x.pubDate }
   end
 end
